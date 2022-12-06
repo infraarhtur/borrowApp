89d23190634d061cadb5dc934ee2e945fb59cb65
infraarhtur <infraarhtur@gmail.com>
2022-12-05 21:48:18 -0500

    lm:se quita archivo que no es de esta aplicacion y opcion de lista

--- src/app/enums/typeDebt.enum.ts
+++ src/app/enums/typeDebt.enum.ts
@@ -1,7 +1,6 @@
 export enum typeDebtEnum {
   sinIntereses = 'Sin intereses',
   interesFijo = 'Interes fijo',
-  interesijoPorCiclo = 'Interes fijo por ciclo',
   interesCompuesto = 'Interes compuesto',
   arriendo = 'Arriendo',
   clase = 'Clase'
