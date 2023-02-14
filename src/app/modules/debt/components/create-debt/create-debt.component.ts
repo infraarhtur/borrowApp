f3c23b1ecc63728bc30a81879b9469d0a2c9d4ff
infraarhtur <infraarhtur@gmail.com>
2023-02-13 22:12:35 -0500

    click en el calendar open

--- src/app/modules/debt/components/create-debt/create-debt.component.ts
+++ src/app/modules/debt/components/create-debt/create-debt.component.ts
@@ -18,6 +18,7 @@ export class CreateDebtComponent implements OnInit {
   frmCreateDebt: FormGroup;
   userInfo:      any;
   contactParam = null;
+  isDisabledPaymentDay = true;
 
   typeDebtEnumKeys     = [];
   PaymentCicleEnumKeys = [];
